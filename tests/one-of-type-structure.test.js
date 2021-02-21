04db4baa4076e3a2573dddd5c7a67598f1300389
Christian Micle <cmicle@inq.com>
2021-02-21 18:25:07 -0500

    more complex unit test

--- tests/one-of-type-structure.test.js
+++ tests/one-of-type-structure.test.js
@@ -140,83 +140,112 @@ describe('Shape Structure', () => {
             const testReqAttAValue = 'A';
             const testReqAttBValue = 'B';
             const testReqAttCValue = 'C';
+            const testReqAttDValue = 'D';
 
             const stringAttributeValue = 42;
             const numberAttributeValue = 43;
             const shapeAttributeValue = 44;
+            const commonAttributeValue = 45;
 
             const stringValue = '1';
             const numberValue = 2;
             const shapeValue = '3';
 
-            const structure = Structure.shape({
-                testReqValueA: Structure.string(),
-                testReqValueB: Structure.string(),
-                testReqValueC: Structure.string(),
-                testOneOfType: Structure.oneOfType([
-                    Structure.string()
-                        .attributes({
-                            flagged: Structure.attribute((value, requirements) => {
-                                expect(value).to.eql(stringValue);
-                                expect(requirements).to.eql([testReqAttAValue]);
-
-                                return stringAttributeValue;
-                            })
-                                .setValidator((value, attributeValueResult, requirements) => {
-                                    expect(value).to.eql(stringValue);
-                                    expect(attributeValueResult).to.eql(stringAttributeValue);
-                                    expect(requirements).to.eql([testReqAttAValue]);
-
-                                    return 'Failing Message';
-                                }, true)
-                                .setRequirements(['$parent.testReqValueA']),
-                        }),
-                    Structure.number()
-                        .attributes({
-                            flagged: Structure.attribute((value, requirements) => {
-                                expect(value).to.eql(numberValue);
-                                expect(requirements).to.eql([testReqAttBValue]);
-
-                                return numberAttributeValue;
-                            })
-                                .setValidator((value, attributeValueResult, requirements) => {
-                                    expect(value).to.eql(numberValue);
-                                    expect(attributeValueResult).to.eql(numberAttributeValue);
-                                    expect(requirements).to.eql([testReqAttBValue]);
-
-                                    return 'Failing Message';
-                                }, true)
-                                .setRequirements(['$parent.testReqValueB']),
-                        }),
-                    Structure.shape({
-                        testString: Structure.string()
+            const buildStructure = (commonValue) => {
+                return Structure.shape({
+                    testReqValueA: Structure.string(),
+                    testReqValueB: Structure.string(),
+                    testReqValueC: Structure.string(),
+                    testReqValueD: Structure.string(),
+                    testOneOfType: Structure.oneOfType([
+                        Structure.string()
                             .attributes({
                                 flagged: Structure.attribute((value, requirements) => {
-                                    expect(value).to.eql(shapeValue);
-                                    expect(requirements).to.eql([testReqAttCValue]);
+                                    expect(value).to.eql(stringValue);
+                                    expect(requirements).to.eql([testReqAttAValue]);
 
-                                    return shapeAttributeValue;
+                                    return stringAttributeValue;
                                 })
                                     .setValidator((value, attributeValueResult, requirements) => {
-                                        expect(value).to.eql(shapeValue);
-                                        expect(attributeValueResult).to.eql(shapeAttributeValue);
-                                        expect(requirements).to.eql([testReqAttCValue]);
+                                        expect(value).to.eql(stringValue);
+                                        expect(attributeValueResult).to.eql(stringAttributeValue);
+                                        expect(requirements).to.eql([testReqAttAValue]);
 
                                         return 'Failing Message';
                                     }, true)
-                                    .setRequirements(['$parent.$parent.testReqValueC']),
+                                    .setRequirements(['$parent.testReqValueA']),
                             }),
-                    }),
-                ]),
-            });
+                        Structure.number()
+                            .attributes({
+                                flagged: Structure.attribute((value, requirements) => {
+                                    expect(value).to.eql(numberValue);
+                                    expect(requirements).to.eql([testReqAttBValue]);
+
+                                    return numberAttributeValue;
+                                })
+                                    .setValidator((value, attributeValueResult, requirements) => {
+                                        expect(value).to.eql(numberValue);
+                                        expect(attributeValueResult).to.eql(numberAttributeValue);
+                                        expect(requirements).to.eql([testReqAttBValue]);
+
+                                        return 'Failing Message';
+                                    }, true)
+                                    .setRequirements(['$parent.testReqValueB']),
+                            }),
+                        Structure.shape({
+                            testString: Structure.string()
+                                .attributes({
+                                    flagged: Structure.attribute((value, requirements) => {
+                                        expect(value).to.eql(shapeValue);
+                                        expect(requirements).to.eql([testReqAttCValue]);
+
+                                        return shapeAttributeValue;
+                                    })
+                                        .setValidator((
+                                            value,
+                                            attributeValueResult,
+                                            requirements,
+                                        ) => {
+                                            expect(value).to.eql(shapeValue);
+                                            expect(attributeValueResult)
+                                                .to.eql(shapeAttributeValue);
+                                            expect(requirements).to.eql([testReqAttCValue]);
+
+                                            return 'Failing Message';
+                                        }, true)
+                                        .setRequirements(['$parent.$parent.testReqValueC']),
+                                }),
+                        }),
+                    ])
+                        .attributes({
+                            common: Structure.attribute((value, requirements) => {
+                                expect(value).to.eql(commonValue);
+                                expect(requirements).to.eql([testReqAttDValue]);
+
+                                return commonAttributeValue;
+                            })
+                                .setValidator((value, attributeValueResult, requirements) => {
+                                    expect(value).to.eql(commonValue);
+                                    expect(attributeValueResult).to.eql(commonAttributeValue);
+                                    expect(requirements).to.eql([testReqAttDValue]);
+
+                                    return 'Failing Message';
+                                }, true)
+                                .setRequirements(['$parent.testReqValueD']),
+                        }),
+                });
+            };
 
             it('When string used', () => {
                 const value = {
                     testReqValueA: testReqAttAValue,
                     testReqValueB: testReqAttBValue,
                     testReqValueC: testReqAttCValue,
+                    testReqValueD: testReqAttDValue,
                     testOneOfType: stringValue,
                 };
+
+                const structure = buildStructure(value.testOneOfType);
 
                 return structure.run(value).then((result) => {
                     expect(result).to.eql({
@@ -231,6 +260,10 @@ describe('Shape Structure', () => {
                             $a: {},
                         },
                         testReqValueC: {
+                            $r: 'pass',
+                            $a: {},
+                        },
+                        testReqValueD: {
                             $r: 'pass',
                             $a: {},
                         },
@@ -242,41 +275,8 @@ describe('Shape Structure', () => {
                                     result: 'fatal',
                                     message: 'Failing Message',
                                 },
-                            },
-                        },
-                    });
-                });
-            });
-
-            it('When string used', () => {
-                const value = {
-                    testReqValueA: testReqAttAValue,
-                    testReqValueB: testReqAttBValue,
-                    testReqValueC: testReqAttCValue,
-                    testOneOfType: stringValue,
-                };
-
-                return structure.run(value).then((result) => {
-                    expect(result).to.eql({
-                        $r: 'fatal',
-                        $a: {},
-                        testReqValueA: {
-                            $r: 'pass',
-                            $a: {},
-                        },
-                        testReqValueB: {
-                            $r: 'pass',
-                            $a: {},
-                        },
-                        testReqValueC: {
-                            $r: 'pass',
-                            $a: {},
-                        },
-                        testOneOfType: {
-                            $r: 'fatal',
-                            $a: {
-                                flagged: {
-                                    value: stringAttributeValue,
+                                common: {
+                                    value: commonAttributeValue,
                                     result: 'fatal',
                                     message: 'Failing Message',
                                 },
@@ -291,8 +291,11 @@ describe('Shape Structure', () => {
                     testReqValueA: testReqAttAValue,
                     testReqValueB: testReqAttBValue,
                     testReqValueC: testReqAttCValue,
+                    testReqValueD: testReqAttDValue,
                     testOneOfType: numberValue,
                 };
+
+                const structure = buildStructure(value.testOneOfType);
 
                 return structure.run(value).then((result) => {
                     expect(result).to.eql({
@@ -310,11 +313,20 @@ describe('Shape Structure', () => {
                             $r: 'pass',
                             $a: {},
                         },
+                        testReqValueD: {
+                            $r: 'pass',
+                            $a: {},
+                        },
                         testOneOfType: {
                             $r: 'fatal',
                             $a: {
                                 flagged: {
                                     value: numberAttributeValue,
+                                    result: 'fatal',
+                                    message: 'Failing Message',
+                                },
+                                common: {
+                                    value: commonAttributeValue,
                                     result: 'fatal',
                                     message: 'Failing Message',
                                 },
@@ -329,10 +341,13 @@ describe('Shape Structure', () => {
                     testReqValueA: testReqAttAValue,
                     testReqValueB: testReqAttBValue,
                     testReqValueC: testReqAttCValue,
+                    testReqValueD: testReqAttDValue,
                     testOneOfType: {
                         testString: shapeValue,
                     },
                 };
+
+                const structure = buildStructure(value.testOneOfType);
 
                 return structure.run(value).then((result) => {
                     expect(result).to.eql({
@@ -350,9 +365,19 @@ describe('Shape Structure', () => {
                             $r: 'pass',
                             $a: {},
                         },
+                        testReqValueD: {
+                            $r: 'pass',
+                            $a: {},
+                        },
                         testOneOfType: {
                             $r: 'fatal',
-                            $a: {},
+                            $a: {
+                                common: {
+                                    value: commonAttributeValue,
+                                    result: 'fatal',
+                                    message: 'Failing Message',
+                                },
+                            },
                             testString: {
                                 $r: 'fatal',
                                 $a: {
