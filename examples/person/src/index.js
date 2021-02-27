9f4086065ed68c97dbda42b9950fe1faaace6e92
Christian Micle <cmicle@metlife.com>
2021-02-27 11:12:00 -0500

    fix

--- examples/person/src/index.js
+++ examples/person/src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import app from "./app";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
